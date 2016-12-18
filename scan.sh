c788b635cb9f13624c0aa4cee5749562b2e23d56
TLeconte <thierry.leconte@adsbnetwork.com>
2016-12-18 15:00:31 +0100

    use 8 channels scan

--- scan.sh
+++ scan.sh
@@ -1,15 +1,17 @@
 #/bin/bash
 
+mkdir -p scan
+
 let fm=128
 while [ "$fm" -lt 139 ] ; do
-let f0=0; let f1=250 ; let f2=500 ; let f3=750
-while [ "$f0" -lt 250 ] ; do
+let f0=0; let f1=125 ; let f2=250 ; let f3=375 ; let f4=500 ; let f5=625 ; let f6=750 ; let f7=875 
+while [ "$f0" -lt 125 ] ; do
 	echo $fm.$f0
-	./acarsdec -l scan/$fm.$f0 -r 0 $fm.$f0 $fm.$f1 $fm.$f2 $fm.$f3 &
-	sleep 800
+	./acarsdec -l scan/$fm.$f0 -r 0 $fm.$f0 $fm.$f1 $fm.$f2 $fm.$f3 $fm.$f4 $fm.$f5 $fm.$f6 $fm.$f7 &
+	sleep 300
 	killall acarsdec
-	sleep 5
-	let f0=f0+25;let f1=f1+25;let f2=f2+25;let f3=f3+25;
+	sleep 2
+	let f0=f0+25;let f1=f1+25;let f2=f2+25;let f3=f3+25; f4=f4+25;let f5=f5+25;let f6=f6+25;let f7=f7+25;
  done
  let fm=fm+1;
 done
