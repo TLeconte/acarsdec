dee8cd992083c879a8d55e3e8662ac7add178c60
leconte <thierry.leconte35@free.fr>
2022-05-18 10:30:29 +0000

    switch to Async MQTT

--- mqttout.c
+++ mqttout.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "MQTTClient.h"
+#include "MQTTAsync.h"
 
 
-static  MQTTClient client;
-static  MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-static  MQTTClient_message pubmsg = MQTTClient_message_initializer;
+static  MQTTAsync client;
+static  MQTTAsync_message pubmsg = MQTTAsync_message_initializer;
 static  char *msgtopic;
 
 int MQTTinit(char **urls, char * client_id, char *topic, char *user,char *passwd)
 {
     int rc;
+    MQTTAsync_createOptions create_opts = MQTTAsync_createOptions_initializer;
+    MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
 
-    MQTTClient_create(&client, urls[0], client_id, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    create_opts.maxBufferedMessages=200;
+    create_opts.allowDisconnectedSendAtAnyTime=1;
+    create_opts.deleteOldestMessages=1;
+
+    MQTTAsync_createWithOptions(&client, urls[0], client_id, MQTTCLIENT_PERSISTENCE_NONE, NULL, &create_opts);
+
     conn_opts.keepAliveInterval = 60;
     conn_opts.cleansession = 1;
+    conn_opts.automaticReconnect = 1;
     conn_opts.password = passwd;
     conn_opts.username = user;
     if(urls[1]) conn_opts.serverURIs = urls;
 
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+    if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS) {
         return(rc);
     }
 
@@ -29,26 +36,25 @@ int MQTTinit(char **urls, char * client_id, char *topic, char *user,char *passwd
     	sprintf(msgtopic,"acarsdec/%s",client_id);
     } else
 	msgtopic=topic;
-	
+
     return rc;
 }
 
 
 int MQTTsend(char *msgtxt)
 {
-    MQTTClient_deliveryToken token;
 
     pubmsg.payload = msgtxt;
     pubmsg.payloadlen = strlen(msgtxt);
     pubmsg.qos = 0;
     pubmsg.retained = 0;
 
-    MQTTClient_publishMessage(client, msgtopic, &pubmsg, &token);
-    return MQTTClient_waitForCompletion(client, token, 2000);
+    return MQTTAsync_sendMessage(client, msgtopic, &pubmsg,NULL);
+
 }
 
 void MQTTend() 
 {
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
+    MQTTAsync_disconnect(client,NULL);
+    MQTTAsync_destroy(&client);
 }
