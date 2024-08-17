TLeconte/acarsdec

commit cec01f571fab8c1e7a979adb5d2bfa83bcdf9d3f

quick ugly hack to allow building multiple backends
- move common global variables out of #define scope
- exit with an error if more than one input is selected
- reformat the usage help text
- rename rtlMult -> rateMult (in line with soapy)
- adjust rtl and soapy gain routines to work with floats

cmake -Drtl=ON -Dsoapy=ON -Dairspy=ON works and builds

acarsdec.c
@@ -25,6 +25,7 @@
 #include <sched.h>
 #include <unistd.h>
 #include <limits.h>
+#include <err.h>
 #ifdef HAVE_LIBACARS
 #include <libacars/version.h>
 #endif
@@ -51,27 +52,19 @@ int signalExit = 0;
 int skip_reassembly = 0;
 #endif
 
-#ifdef WITH_RTL
-int gain = -100;
+float gain = 0;
 int ppm = 0;
-int rtlMult = 160;
+int rateMult = 160;
+#ifdef WITH_RTL
 int bias = 0;
-#endif
-
-#ifdef WITH_AIR
-int gain = 18;
 #endif
 
 #ifdef	WITH_SDRPLAY
 int	lnaState	= 2;
 int	GRdB		= 20;
-int	ppm		= 0;
 #endif
 #ifdef WITH_SOAPY
 char *antenna=NULL;
-double gain = -10.0;
-int ppm = 0;
-int rateMult = 160;
 int freq = 0;
 #endif
 
@@ -112,18 +105,19 @@ static void usage(void)
 #endif
 #ifdef WITH_RTL
 	fprintf(stderr,
-		" [-g gain] [-p ppm] [-B bias] -r rtldevicenumber  f1 [f2] ... [fN]");
+		" -r rtldevicenumber [rtlopts] |");
 #endif
 #ifdef WITH_AIR
 	fprintf(stderr,
-		"[-g linearity_gain] -s airspydevicenumber f1 [f2] ... [fN]");
+		" -s airspydevicenumber [airspyopts] |");
 #endif
 #ifdef	WITH_SDRPLAY
-	fprintf (stderr, " [-L lnaState] [-G GRdB] [-p ppm] -s f1 [f2] .. [fN]");
+	fprintf (stderr, " -s [sdrplayopts] |");
 #endif
 #ifdef	WITH_SOAPY
-	fprintf (stderr, " [--antenna antenna] [-g gain] [-p ppm] [-c freq] -d devicestring f1 [f2] .. [fN]");
+	fprintf (stderr, " -d devicestring [soapyopts]");
 #endif
+	fprintf (stderr, " f1 [f2] .. [fN]");
 	fprintf(stderr, "\n\n");
 #ifdef HAVE_LIBACARS
 	fprintf(stderr, " --skip-reassembly\t: disable reassembling fragmented ACARS messages\n");
@@ -170,40 +164,44 @@ static void usage(void)
 		" -f inputwavfile\t: decode from a wav file at %d sampling rate\n",INTRATE);
 #endif
 #ifdef WITH_RTL
+	fprintf(stderr, "\n rtlopts:\n");
 	fprintf(stderr,
 		" -g gain\t\t: set rtl gain in db (0 to 49.6; >52 and -10 will result in AGC; default is AGC)\n");
 	fprintf(stderr, " -p ppm\t\t\t: set rtl ppm frequency correction\n");
-	fprintf(stderr, " -m rtlMult\t\t\t: set rtl sample rate multiplier: 160 for 2 MS/s or 192 for 2.4 MS/s (default: 160)\n");
+	fprintf(stderr, " -m rateMult\t\t\t: set rtl sample rate multiplier: 160 for 2 MS/s or 192 for 2.4 MS/s (default: 160)\n");
 	fprintf(stderr, " -B bias\t\t\t: Enable (1) or Disable (0) the bias tee (default is 0)\n");
 	fprintf(stderr,
 		" -r rtldevice f1 [f2]...[f%d]\t: decode from rtl dongle number or S/N rtldevice receiving at VHF frequencies f1 and optionally f2 to f%d in Mhz (ie : -r 0 131.525 131.725 131.825 )\n", MAXNBCHANNELS, MAXNBCHANNELS);
 #endif
 #ifdef WITH_AIR
+	fprintf(stderr, "\n airspyopts:\n");
 	fprintf(stderr,
 		" -g linearity_gain\t: set linearity gain [0-21] default : 18\n");
 	fprintf(stderr,
 		" -s airspydevice f1 [f2]...[f%d]\t: decode from airspy dongle number or hex serial number receiving at VHF frequencies f1 and optionally f2 to f%d in Mhz (ie : -s 131.525 131.725 131.825 )\n", MAXNBCHANNELS, MAXNBCHANNELS);
 #endif
 #ifdef	WITH_SDRPLAY
+	fprintf(stderr, "\n sdrplayopts:\n");
 	fprintf (stderr,
 	          "-L lnaState: set the lnaState (depends on the device)\n"\
 	          "-G Gain reducction in dB's, range 20 .. 59 (-100 is autogain)\n"\
 	          " -s f1 [f2]...[f%d]\t: decode from sdrplay receiving at VHF frequencies f1 and optionally f2 to f%d in Mhz (ie : -s 131.525 131.725 131.825 )\n", MAXNBCHANNELS, MAXNBCHANNELS);
 #endif
 #ifdef	WITH_SOAPY
-	fprintf(stderr, 
+	fprintf(stderr, "\n soapyopts:\n");
+	fprintf(stderr,
 		" --antenna antenna\t: set antenna port to use\n");
 	fprintf(stderr,
 		" -g gain\t\t: set gain in db (-10 will result in AGC; default is AGC)\n");
 	fprintf(stderr, " -p ppm\t\t\t: set ppm frequency correction\n");
-	fprintf(stderr, " c freq\t\t\t: set center frequency to tune to\n");
+	fprintf(stderr, " -c freq\t\t: set center frequency to tune to\n");
 	fprintf(stderr, " -m rateMult\t\t\t: set sample rate multiplier: 160 for 2 MS/s or 192 for 2.4 MS/s (default: 160)\n");
 	fprintf (stderr,
 		" -d devicestring f1 [f2] .. [f%d]\t: decode from a SoapySDR device located by devicestring at VHF frequencies f1 and optionally f2 to f%d in Mhz (ie : -d driver=rtltcp 131.525 131.725 131.825 )\n", MAXNBCHANNELS, MAXNBCHANNELS);
 #endif
 
 	fprintf(stderr,
-		" Up to %d channels may be simultaneously decoded\n", MAXNBCHANNELS);
+		"\n Up to %d channels may be simultaneously decoded\n", MAXNBCHANNELS);
 	exit(1);
 }
 
@@ -266,29 +264,35 @@ int main(int argc, char **argv)
 #endif
 #ifdef WITH_ALSA
 		case 'a':
+			if (inmode)
+				errx(-1, "Only 1 input allowed");
 			res = initAlsa(argv, optind);
 			inmode = 1;
 			break;
 #endif
 #ifdef WITH_SNDFILE
 		case 'f':
+			if (inmode)
+				errx(-1, "Only 1 input allowed");
 			res = initSoundfile(argv, optind);
 			inmode = 2;
 			break;
 #endif
-#ifdef WITH_RTL
-		case 'r':
-			res = initRtl(argv, optind);
-			inmode = 3;
+		case 'g':
+			gain = atof(optarg);
 			break;
 		case 'p':
 			ppm = atoi(optarg);
 			break;
-		case 'g':
-			gain = 10 * atof(optarg);
-			break;
 		case 'm':
-			rtlMult = atoi(optarg);
+			rateMult = atoi(optarg);
+			break;
+#ifdef WITH_RTL
+		case 'r':
+			if (inmode)
+				errx(-1, "Only 1 input allowed");
+			res = initRtl(argv, optind);
+			inmode = 3;
 			break;
 		case 'B':
 			bias = atoi(optarg);
@@ -296,11 +300,10 @@ int main(int argc, char **argv)
 #endif
 #ifdef	WITH_SDRPLAY
 		case 's':
+			if (inmode)
+				errx(-1, "Only 1 input allowed");
 			res = initSdrplay (argv, optind);
 			inmode = 5;
-			break;
-		case 'p':
-			ppm = atoi(optarg);
 			break;
     case 'L':
 			lnaState = atoi(optarg);
@@ -314,29 +317,21 @@ int main(int argc, char **argv)
 			antenna = optarg;
 			break;
 		case 'd':
+			if (inmode)
+				errx(-1, "Only 1 input allowed");
 			res = initSoapy(argv, optind);
 			inmode = 6;
-			break;
-		case 'p':
-			ppm = atoi(optarg);
 			break;
 		case 'c':
 			freq = atoi(optarg);
 			break;
-		case 'g':
-			gain = atof(optarg);
-			break;
-		case 'm':
-			rateMult = atoi(optarg);
-			break;
 #endif
 #ifdef WITH_AIR
 		case 's':
+			if (inmode)
+				errx(-1, "Only 1 input allowed");
 			res = initAirspy(argv, optind);
 			inmode = 4;
-			break;
-    		case 'g':
-			gain = atoi(optarg);
 			break;
 #endif
 #ifdef WITH_MQTT
@@ -486,12 +481,16 @@ int main(int argc, char **argv)
 #endif
 #ifdef WITH_RTL
 	case 3:
+		if (!gain)
+			gain = -10;
 		runRtlSample();
 		res = runRtlClose();
 		break;
 #endif
 #ifdef WITH_AIR
 	case 4:
+		if (!gain)
+			gain = 18;
 		res = runAirspySample();
 		break;
 #endif
@@ -502,6 +501,8 @@ int main(int argc, char **argv)
 #endif
 #ifdef WITH_SOAPY
 	case 6:
+		if (!gain)
+			gain = -10;
 		runSoapySample();
 		res = runSoapyClose();
 		break;

acarsdec.h
@@ -64,13 +64,9 @@ typedef struct {
 	int chn;
 
 #if defined(WITH_RTL) || defined(WITH_AIR)
-	int Fr;
 	float complex *wf;
 #endif
-#if defined(WITH_AIR)
-	float complex D;
-#endif
-#if defined(WITH_SDRPLAY) || defined(WITH_SOAPY)
+#if defined(WITH_RTL) || defined(WITH_SDRPLAY) || defined(WITH_SOAPY) || defined(WITH_AIR)
 	float	Fr;
         float	complex *oscillator;
         float	complex D;
@@ -145,8 +141,10 @@ extern int emptymsg;
 extern int mdly;
 extern int hourly, daily;
 
+extern float gain;
 extern int ppm;
 extern int bias;
+extern int rateMult;
 extern	int	lnaState;
 extern	int	GRdB;
 extern int initOutput(char*,char *);
@@ -167,7 +165,6 @@ extern int initRtl(char **argv,int optind);
 extern int runRtlSample(void);
 extern int runRtlCancel(void);
 extern int runRtlClose(void);
-extern int rtlMult;
 #endif
 #ifdef WITH_AIR
 extern int initAirspy(char **argv,int optind);
@@ -178,11 +175,7 @@ extern int initSoapy(char **argv,int optind);
 extern int soapySetAntenna(const char *antenna);
 extern int runSoapySample(void);
 extern int runSoapyClose(void);
-extern int rateMult;
 extern int freq;
-extern double gain;
-#else
-extern int gain;
 #endif
 #ifdef WITH_MQTT
 extern int MQTTinit(char **urls, char * client_id, char *topic, char *user,char *passwd);

air.c
@@ -245,7 +245,7 @@ int initAirspy(char **argv, int optind)
        /* enable packed samples */
         airspy_set_packing(device, 1);
 
-	result = airspy_set_linearity_gain(device, gain);
+	result = airspy_set_linearity_gain(device, (int)gain);
 	if( result != AIRSPY_SUCCESS ) {
 		fprintf(stderr,"airspy_set_vga_gain() failed: %s (%d)\n", airspy_error_name(result), result);
 	}
@@ -282,7 +282,7 @@ int initAirspy(char **argv, int optind)
 		}
 		ch->D=0;
 
-		AMFreq = 2.0*M_PI*(double)(Fc-ch->Fr+AIRINRATE/4)/(double)(AIRINRATE);
+		AMFreq = 2.0*M_PI*(Fc-ch->Fr+(double)AIRINRATE/4)/(double)(AIRINRATE);
 		for (i = 0, Ph=0; i < AIRMULT; i++) {
 			ch->wf[i]=cexpf(Ph*-I)/AIRMULT;
 			Ph+=AMFreq;

rtl.c
@@ -32,9 +32,9 @@
 // set the sameple rate by changing RTMULT
 // 2.5Ms/s is the best but could be over limit for some hardware
 // 2.0Ms/s is safer
-// rtlMult 160	// 2.0000 Ms/s
-// rtlMult 192	// 2.4000 Ms/s
-// rtlMult 200   // 2.5000 Ms/s
+// rateMult 160	// 2.0000 Ms/s
+// rateMult 192	// 2.4000 Ms/s
+// rateMult 200   // 2.5000 Ms/s
 
 #define RTLMULTMAX 320 // this is well beyond the rtl-sdr capabilities
 
@@ -209,13 +209,13 @@ int initRtl(char **argv, int optind)
 	dev_index = verbose_device_search(argv[optind]);
 	optind++;
 
-	if (rtlMult > RTLMULTMAX) {
-		fprintf(stderr, "rtlMult can't be larger than 360\n");
+	if (rateMult > RTLMULTMAX) {
+		fprintf(stderr, "rateMult can't be larger than 360\n");
 		return 1;
 	}
 
-    rtlInBufSize = RTLOUTBUFSZ * rtlMult * 2;
-    rtlInRate = INTRATE * rtlMult;
+    rtlInBufSize = RTLOUTBUFSZ * rateMult * 2;
+    rtlInRate = INTRATE * rateMult;
 
 	r = rtlsdr_open(&dev, dev_index);
 	if (r < 0) {
@@ -223,16 +223,17 @@ int initRtl(char **argv, int optind)
 		return r;
 	}
 
-	if (gain > 520 || gain == -100) {
+	if (gain > 52.0F || gain <= 10.0F) {
 		if (verbose)
 			fprintf(stderr, "Tuner gain: AGC\n");
 		r = rtlsdr_set_tuner_gain_mode(dev, 0);
 	} else {
 		rtlsdr_set_tuner_gain_mode(dev, 1);
-        gain = nearest_gain(gain);
+        gain = nearest_gain((int)(gain*10.0F));
+	gain /= 10.0F;
         if (verbose)
-            fprintf(stderr, "Tuner gain: %f\n", (float)gain / 10.0);
-		r = rtlsdr_set_tuner_gain(dev, gain);
+            fprintf(stderr, "Tuner gain: %.1f\n", gain);
+		r = rtlsdr_set_tuner_gain(dev, (int)(gain*10.0F));
 	}
 	if (r < 0)
 		fprintf(stderr, "WARNING: Failed to set gain.\n");
@@ -278,15 +279,15 @@ int initRtl(char **argv, int optind)
 		int ind;
 		float AMFreq;
 
-		ch->wf = malloc(rtlMult * sizeof(float complex));
+		ch->wf = malloc(rateMult * sizeof(float complex));
 		ch->dm_buffer=malloc(RTLOUTBUFSZ*sizeof(float));
 		if( ch->wf == NULL || ch->dm_buffer == NULL) {
 			fprintf(stderr, "ERROR : malloc\n");
 			return 1;
 		}
 		AMFreq = (ch->Fr - (float)Fc) / (float)(rtlInRate) * 2.0 * M_PI;
-		for (ind = 0; ind < rtlMult; ind++) {
-			ch->wf[ind]=cexpf(AMFreq*ind*-I)/rtlMult/127.5;
+		for (ind = 0; ind < rateMult; ind++) {
+			ch->wf[ind]=cexpf(AMFreq*ind*-I)/rateMult/127.5;
 		}
 	}
 
@@ -338,12 +339,12 @@ static void in_callback(unsigned char *rtlinbuff, uint32_t nread, void *ctx)
 	status=0;
 
 	// code requires this relationship set in initRtl:
-	// rtlInBufSize = RTLOUTBUFSZ * rtlMult * 2;
+	// rtlInBufSize = RTLOUTBUFSZ * rateMult * 2;
 
 	float complex vb[RTLMULTMAX];
 	int i = 0;
 	for (int m = 0; m < RTLOUTBUFSZ; m++) {
-		for (int ind = 0; ind < rtlMult; ind++) {
+		for (int ind = 0; ind < rateMult; ind++) {
 			float r, g;
 
 			r = (float)rtlinbuff[i] - 127.37f; i++;
@@ -358,7 +359,7 @@ static void in_callback(unsigned char *rtlinbuff, uint32_t nread, void *ctx)
 
 			wf = ch->wf;
 			D = 0;
-			for (int ind = 0; ind < rtlMult; ind++) {
+			for (int ind = 0; ind < rateMult; ind++) {
 				D += vb[ind] * wf[ind];
 			}
 			ch->dm_buffer[m]=cabsf(D);

soapy.c
@@ -90,7 +90,7 @@ int initSoapy(char **argv, int optind)
 
 	soapyInBuf = malloc(sizeof(int16_t) * soapyInBufSize);
 
-	if (gain == -10.0) {
+	if (gain <= -10.0) {
 		if (verbose)
 			fprintf(stderr, "Tuner gain: AGC\n");
 		r = SoapySDRDevice_setGainMode(dev, SOAPY_SDR_RX, 0, 1);

commit 0e09bd120abb10370fe398e346cfcd70569b08b5

acarsdec: fix signal handling
Fixes: cec01f571

acarsdec.c
@@ -155,11 +155,11 @@ static void sigintHandler(int signum)
 #endif
 #ifdef WITH_RTL
 	runRtlCancel();
-#elif WITH_SOAPY
-	runSoapyClose();
-#else
-	exit(0);
 #endif
+#ifdef WITH_SOAPY
+	runSoapyClose();
+#endif
+	exit(0);
 }
 
 int initMsk(channel_t *);