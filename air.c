TLeconte/acarsdec

commit 4cf0abeaae6e878db172632921c4f1d2eeaeaced

airspy: simplify code by using CF32 samples
it would seem the previous code wasn't working
after this commit i can get decodes

air.c
@@ -169,8 +169,7 @@ int initAirspy(char *optarg)
 
 	/* init airspy */
 
-	// XXX REVIEW: airspy can do AIRSPY_SAMPLE_FLOAT32_IQ, like everyone else. Is there a reason to use real?
-	result = airspy_set_sample_type(device, AIRSPY_SAMPLE_FLOAT32_REAL);
+	result = airspy_set_sample_type(device, AIRSPY_SAMPLE_FLOAT32_IQ);
 	if (result != AIRSPY_SUCCESS) {
 		fprintf(stderr, "airspy_set_sample_type() failed: %s (%d)\n", airspy_error_name(result), result);
 		airspy_close(device);
@@ -247,86 +246,16 @@ int initAirspy(char *optarg)
 	}
 	vprerr("Set freq. to %d hz\n", Fc);
 
-	/* computes mixers osc. */
-	for (n = 0; n < R.nbch; n++) {
-		channel_t *ch = &(R.channels[n]);
-		int i;
-		double AMFreq, Ph;
-
-		ch->oscillator = malloc(AIRMULT * sizeof(*ch->oscillator));
-		ch->dm_buffer = malloc(DMBUFSZ * sizeof(*ch->dm_buffer));
-		if (ch->oscillator == NULL || ch->dm_buffer == NULL) {
-			perror(NULL);
-			airspy_close(device);
-			airspy_exit();
-			return -1;
-		}
-
-		AMFreq = 2.0 * M_PI * (Fc - ch->Fr + (double)AIRINRATE / 4) / (double)(AIRINRATE);
-		for (i = 0, Ph = 0; i < AIRMULT; i++) {
-			ch->oscillator[i] = cexpf(Ph * -I) / AIRMULT;
-			Ph += AMFreq;
-			if (Ph > 2.0 * M_PI)
-				Ph -= 2.0 * M_PI;
-			if (Ph < -2.0 * M_PI)
-				Ph += 2.0 * M_PI;
-		}
-	}
+	result = channels_init_sdr(Fc, AIRMULT, 1.0F);
+	if (result)
+		return result;
 
 	return 0;
 }
 
 static int rx_callback(airspy_transfer_t *transfer)
 {
-	static int ind = 0;
-	float *pt_rx_buffer;
-	int n, i;
-	int bo, be, ben, nbk;
-
-	pt_rx_buffer = (float *)(transfer->samples);
-
-	bo = AIRMULT - ind;
-	nbk = (transfer->sample_count - bo) / AIRMULT;
-	be = nbk * AIRMULT + bo;
-	ben = transfer->sample_count - be;
-
-	for (n = 0; n < R.nbch; n++) {
-		channel_t *ch = &(R.channels[n]);
-		float S;
-		int k, bn, m;
-		float complex D;
-
-		D = chD[n];
-
-		/* compute */
-		m = 0;
-		k = 0;
-		for (i = ind; i < AIRMULT; i++, k++) {
-			S = pt_rx_buffer[k];
-			D += ch->oscillator[i] * S;
-		}
-		ch->dm_buffer[m++] = cabsf(D);
-
-		for (bn = 0; bn < nbk; bn++) {
-			D = 0;
-			for (i = 0; i < AIRMULT; i++, k++) {
-				S = pt_rx_buffer[k];
-				D += ch->oscillator[i] * S;
-			}
-			ch->dm_buffer[m++] = cabsf(D);
-		}
-
-		D = 0;
-		for (i = 0; i < ben; i++, k++) {
-			S = pt_rx_buffer[k];
-			D += ch->oscillator[i] * S;
-		}
-		chD[n] = D;
-
-		demodMSK(ch, m);
-	}
-	ind = ben;
-
+	channels_mix_phasors(transfer->samples, transfer->sample_count, AIRMULT);
 	return 0;
 }
 

commit 052e307b0b76f9077137226c399b2a2f5e348768

airspy: remove now unused global variable
Fixes: 4cf0abe

air.c
@@ -37,7 +37,6 @@ static struct airspy_device *device = NULL;
 
 static const unsigned int r820t_hf[] = { 1953050, 1980748, 2001344, 2032592, 2060291, 2087988 };
 static const unsigned int r820t_lf[] = { 525548, 656935, 795424, 898403, 1186034, 1502073, 1715133, 1853622 };
-static float complex *chD;
 
 static unsigned int chooseFc(unsigned int minF, unsigned int maxF, int filter)
 {
@@ -87,12 +86,6 @@ int initAirspy(char *optarg)
 
 	if (!R.gain)
 		R.gain = 18;
-
-	chD = calloc(R.nbch, sizeof(*chD));
-	if (!chD) {
-		perror(NULL);
-		return -1;
-	}
 
 	// Request the total number of libairspy devices connected, allocate space, then request the list.
 	result = airspy_device_count = airspy_list_devices(NULL, 0);
@@ -281,8 +274,6 @@ int runAirspySample(void)
 	}
 
 	airspy_stop_rx(device);
-
-	free(chD);
 
 	return 0;
 }